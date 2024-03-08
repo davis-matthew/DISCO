e64e773079845ed5ee3f87b5a72f160ec9c80919
davis-matthew <mdavis438@gatech.edu>
2024-03-08 11:59:57 -0500

    Add disco description

--- src/disco.cpp
+++ src/disco.cpp
@@ -2,7 +2,24 @@
 
 namespace llvm { 
 
-PreservedAnalyses Disco::run(Module &M, ModuleAnalysisManager &MAM){
+PreservedAnalyses Disco::run(Module &M, ModuleAnalysisManager &MAM) {
+
+  // generate global data structure (dictionary) from replacements.txt
+
+  // wrap puts() or other print functions with something like:
+  
+  /* 
+  void newPuts(char* s){
+    checkStringSpelling(s);
+    puts(s);
+  }
+  void checkStringSpelling(char* s){
+    for key in globaldictionary:
+      if s.lowercase().find(key.lowercase()) != string::npos:
+        throw exception("Found Spelling Error! {s} -> {s.replace(key, globaldictionary[key])}");
+  }
+  */
+
   return PreservedAnalyses::all();
 }
 
