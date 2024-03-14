86911da8b31042cb65850b843f31185aeaeff617
davis-matthew <mdavis438@gatech.edu>
2024-03-14 13:48:24 -0400

    Add initial atexit() instrumentation implementation

--- src/disco.cpp
+++ src/disco.cpp
@@ -17,15 +17,27 @@ Find all instances of puts() or other print functions and stick a call to our fu
 */
 
 PreservedAnalyses Disco::run(Module &M, ModuleAnalysisManager &MAM) {
+  LLVMContext &globalContext = M.getContext();
 
+  // Insert call to atexit(printSpellcheckStats)
+  std::vector<Type*> spellCheckStatsParameters = {Type::getVoidTy(globalContext)};
+  FunctionType *spellCheckStatsFuncType = FunctionType::get(Type::getVoidType(globalContext), spellCheckStatsParameters, false);
+  Function *spellCheckStatsFunc = cast<Function>(M.getOrInsertFunction("printSpellcheckStats"), spellCheckStatsFuncType).getCallee();
+  
+  std::vector<Type*> atExitParameters = {PointerType::getUnqual(spellCheckStatsFuncType)};
+  FunctionType *atExitFuncType = FunctionType::get(Type::getVoidType(globalContext), atExitParameters, false);
+  Function *atExitFunc = cast<Function>(M.getOrInsertFunction("atexit"), atExitFuncType).getCallee();
+
+  CallInst *atExitSpellCheckStats = CallInst::Create(atExitFunc, spellCheckStatsFunc, 
+                                      M.getOrInsertFunction("main").begin().begin()); // Start of main
+
+  // Insert call to checkStringSpelling above each puts() call
   std::unordered_set<std::string> outputFunctions = ({
     "@puts"
   });
 
-  LLVMContext &globalContext = M.getContext();
-
-  std::vector<Type*> parameters = {Type::getInt8PtrTy(globalContext)};
-  FunctionType *spellCheckFuncType = FunctionType::get(Type::getVoidTy(M.getContext()), parameters, false);
+  std::vector<Type*> spellCheckParameters = {Type::getInt8PtrTy(globalContext)};
+  FunctionType *spellCheckFuncType = FunctionType::get(Type::getVoidTy(globalContext), spellCheckParameters, false);
   Function *spellCheckFunc = cast<Function>(M.getOrInsertFunction("checkStringSpelling", spellCheckFuncType).getCallee());
 
   for(Function &F : M)
