f6be6648410a165dd56209122fab9e09f9db1260
davis-matthew <mdavis438@gatech.edu>
2024-03-14 14:24:51 -0400

    Change to unordered map for constant lookup

--- tools/map_generator.py
+++ tools/map_generator.py
@@ -1,7 +1,7 @@
 def generate_cpp_code(input_file, output_file):
     with open(input_file, 'r') as file, open(output_file, 'w') as output:
-        output.write("#include <map>\n#include <string>\n\n")
-        output.write("std::map<std::string, std::string> globalDictionary = {\n")
+        output.write("#include <unordered_map>\n#include <string>\n\n")
+        output.write("std::unordered_map<std::string, std::string> globalDictionary = {\n")
 
         for line in file:
             parts = line.strip().split('->')
