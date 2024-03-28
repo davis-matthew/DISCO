2dbb579c3d4c84c35f9d306ce736253f4f3acea4
Denis Koshelev <truemacintoshchannel@gmail.com>
2024-03-28 15:25:38 -0400

    Add simple test case

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void other() {
+    exit(0);
+}
+
+int main() {
+    puts("i wanna be 1nd");
+    puts("i love this country ameria");
+    puts("there is nothing to do in heidelburg, i am pretty sure");
+    return 0;
+}
